c37dc5ebc8a02db6924a596bd283351a6c94b2a8
Nick Brookes <git@nickbrookes.net>
2023-01-07 15:54:50 +0000

    trying to fix actions errors

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
+"""Module providingFunction printing python version."""
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
@@ -10,6 +10,7 @@ def print_hi(name):
 
 
 # Press the green button in the gutter to run the script.
+"""missing function or method"""
 if __name__ == '__main__':
     print_hi('PyCharm')
 
---
+++ manage.py
@@ -0,0 +1,1 @@
+"""Module providingFunction printing python version."""
